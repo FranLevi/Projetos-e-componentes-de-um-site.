b5f838ca51a57ee3de3e3982c2404e427db9d82e
FranLevi <144162575+FranLevi@users.noreply.github.com>
2023-12-11 16:49:52 -0300

    Site com banner, navbar, cards e footer de empresa
    Navbar, banner, cards e footer de empresa fictícia.

---
+++ siteReactLevi/js/custom.js
@@ -0,0 +1,6 @@
+document.querySelector('#menu-btn').addEventListener('click', function() {
+    //document.write("Menu");
+
+    document.querySelector('#menu-site').classList.toggle("active");
+    document.querySelector('#menu-site').classList.toggle("active");
+});
