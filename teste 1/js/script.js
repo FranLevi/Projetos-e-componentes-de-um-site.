ceb07a6424ea0c7580f22726f4b82071bf5b2b61
FranLevi <144162575+FranLevi@users.noreply.github.com>
2024-02-28 15:21:32 -0300

    Portfolio html,css e js do canal Cryptical Coder
    Um Portfolio que  foi inspirado em vídeo do canal Cryptical Coder

---
+++ teste 1/js/script.js
@@ -0,0 +1,7 @@
+// ==================================== typing animation ==============================
+let typed = new Typed(".typing",{
+    strings:["", "Web Designer","Web Developer","Designer Gráfico", "Fotográfo"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
